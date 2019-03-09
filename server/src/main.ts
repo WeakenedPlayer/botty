b53f71c396fd1a7d6d2a37f1e716692305e42829
weakenedplayer <WeakenedPlayer@gmail.com>
2019-03-09 22:46:39 +0900

    Angularの画面をサーバで表示できるように変更

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create( AppModule );
+    
+    app.useStaticAssets( join( __dirname, '..', 'public') );    // server/public にバンドルしたファイルを置く
+
+    await app.listen( 3000 );   // TODO: コマンドライン引数でポートを変えられるようにする
 }
 bootstrap();
